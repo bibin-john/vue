54ae26d520fe4a806f7b27215dd19329e4d0b0b4
Bibin <bibin3@gmail.com>
2020-09-06 10:33:33 +0530

    multi select

---
+++ src/components/MultiSelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="border:1px solid #ccc;margin-top:20px">
+    <div style="display:flex">
+      <div>{{ titleSection.titleName }}</div>
+      <div>All</div>
+      <div>clear</div>
+      <div>X</div>
+    </div>
+    <div style="display:flex">
+      <div v-for="option in selectOptions" :key="option.name">
+        <input
+          type="checkbox"
+          :checked="option.selected"
+          v-model="option.selected"
+        />
+        {{ option.name }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MultiSelect",
+  props: {
+    titleSection: {
+      type: Object
+    },
+    selectOptions: {
+      type: Array
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,65 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div style="margin:10px auto 10px auto;width:50%">
+      <div v-for="file in episodicData" :key="file.id">
+        <multi-select
+          :titleSection="file.titleSection"
+          :selectOptions="file.selectOptions"
+          ref="multiselect"
+        >
+        </multi-select>
+      </div>
+      <a @click.prevent="getUserSelected()">get</a>
+    </div>
   </div>
 </template>
+<script>
+import MultiSelect from "@/components/MultiSelect";
+export default {
+  name: "about",
+  components: { MultiSelect },
+  data: function() {
+    return {
+      episodicData: [
+        {
+          id: 1,
+          titleSection: { titleName: "music1.pdf" },
+          selectOptions: [
+            { name: "201", id:1, selected: true },
+            { name: "202", id:2, selected: true },
+            { name: "203", id:3, selected: false },
+            { name: "204", id:4, selected: false }
+          ]
+        },
+        {
+          id: 2,
+          titleSection: { titleName: "music2.pdf" },
+          selectOptions: [
+            { name: "201", id:1, selected: false },
+            { name: "202", id:2, selected: false },
+            { name: "203", id:3, selected: true },
+            { name: "204", id:4, selected: true }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    getUserSelected() {
+      
+      let episodicDetails = []
+      this.$refs.multiselect.forEach(value => {
+        let selected = [];
+        value.selectOptions.forEach(value => {
+          value.selected?selected.push(value):null;
+        });
+
+          episodicDetails.push({fileDetails:value.titleSection ,episodicDetails:selected});
+      });
+        
+      console.log(episodicDetails);
+    }
+  }
+};
+</script>
